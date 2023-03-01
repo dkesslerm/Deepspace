1bb4214d74bba8b91b891621c1e2e7791a81318b
dkesslerm <100216259+dkesslerm@users.noreply.github.com>
2023-03-01 10:28:34 +0100

    Delete hola.rb

--- hola.rb
+++
@@ -1,9 +0,0 @@
-#encoding:utf-8
-module Basico
-    module CombatResult
-        ENEMYWINS=:enemywins
-        NOCOMBAT=:nocombat
-        STATIONESCAPES=:stationescapes
-        STATIONWINS=:stationwins
-    end
-end
