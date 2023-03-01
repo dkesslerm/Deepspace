5f745a63654fcf0e2e1e35aa63eee96cd9f5a711
dkesslerm <dkesslerm03@correo.ugr.es>
2023-03-01 09:49:32 +0100

    creacion

---
+++ hola.rb
@@ -0,0 +1,9 @@
+#encoding:utf-8
+module Basico
+    module CombatResult
+        ENEMYWINS=:enemywins
+        NOCOMBAT=:nocombat
+        STATIONESCAPES=:stationescapes
+        STATIONWINS=:stationwins
+    end
+end
